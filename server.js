056607f81a0da9b8ec7f30ddcab07c8bcd24d312
github-account-i-guess <nate@heroesacademy.org>
2022-07-12 13:38:42 -0600

    Steam api stuff

--- server.js
+++ server.js
@@ -1,10 +1,3 @@
-// TYPE THIS INTO YOUR TERMINAL: (without the //)
-// npm install express nodemon
-
-// Run your server using 'npm start' (this will restart your server every time you save)
-
-// If you run into issues, copy and paste an error into google and look for 'stackoverflow'! That will probably explain better than I will! (It's how I learned how to code)
-
 const express = require('express'); // server functions
 const path = require('path'); // getting a directory
 const app = express();
@@ -25,8 +18,56 @@ app.post('/*', (req, res, next) => {
 
 app.use('/static', express.static(path.resolve(__dirname, './static')));
 
-app.get('/*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './templates/your-dashboard.html'));
 });
+app.get("/userGames/:steamId", (req, res) => {
+    //TODO: Don't publish my api key on public github b/c thats really stupid
+    const apiKey = "6C13C7F548AD28C60BECEDF430021DDB"
+    // Getting Axios a library to help with requesting APIs. It's docs can be found here: https://axios-http.com/docs
+    const axios = require('axios')
+
+    //Requesting user account info so I can test whether the user's profile is visible to me (because it is my api key)
+    axios.get("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=" + apiKey + "&steamids=" + req.params.steamId + "&format=json")
+        .then((userData) => {
+            //Testing if the account is visible
+            if (userData.data.response.players[0].communityvisibilitystate == 3) {
+                //Showing friends list
+                /*axios.get("http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=" + apiKey + "&steamid=" + req.params["steamId"] + "&relationship=all&format=json")
+                    .then((friendsList) => {
+                        res.send(friendsList.data)
+                    })
+                    //More "Error Handling"
+                    .catch( e => {
+                        console.error(e)
+                    })*/
+
+                //Showing Owned Games
+                axios.get("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=" + apiKey + "&steamid=" + req.params.steamId + "&format=json&include_appinfo=1&include_played_free_games=1").then ((games) => {
+                    //let strings = []
+                    for (let game of games.data.response.games) {
+                        console.log(game)
+                        axios.get("https://api.isthereanydeal.com/v02/game/plain/?key=2a2fc17c80b93372f570423e56c4eadc04d36934&shop=steam&game_id=app%" + game.appid)
+                            .then((cost) => {
+                                console.log(cost)
+                            })
+                            .catch(e => {
+                                //console.error(e)
+                            })
+                    }
+                    res.send(JSON.stringify(games.data))
+                })
+
+            } else {
+                res.send("No Access")
+            }
+
+        })
+        //"Error Handling"
+        .catch (e => {
+            console.error(e)
+        })
+    //axios.get("http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=" + apiKey + "&steamid=" + req.params["steamId"] + "&format=json&include_appinfo=true&include_played_free_games=true")
+})
 
 app.listen(8080, () => console.log('----------------------------------------------------------\nServer Started on port 8080...'));
